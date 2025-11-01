f6ee0291bf97b567b9c17e323d58bd86d68b6ec5
HwanJung <jh013188@gmail.com>
2025-11-02 00:46:57 +0900

    feat(parser): 사용자 입력을 숫자로 파싱

---
+++ src/main/java/lotto/ui/input/Parser.java
@@ -0,0 +1,35 @@
+package lotto.ui.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+    public int parsePurchaseAmount(String input) {
+        return parseToInt(input);
+    }
+
+    public List<Integer> parseWinningNumbers(String input) {
+        List<String> numbers = new ArrayList<>(List.of(input.split(",", -1)));
+        return makeIntegerNumbers(numbers);
+    }
+
+    public int parseBonusAmount(String input) {
+        return parseToInt(input);
+    }
+
+    private List<Integer> makeIntegerNumbers(List<String> numbers) {
+        return numbers.stream()
+            .map(this::parseToInt)
+            .toList();
+    }
+
+    private int parseToInt(String input) {
+        input = input.trim();
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] Number input is not an integer. input: " + input);
+        }
+    }
+
+}
